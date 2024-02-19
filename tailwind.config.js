1ec7cbf18c3995d229d8b19b0af13e701bc93bea
Mauricio <maurorl200318@gmail.com>
2024-02-18 20:45:57 -0500

    Configuracion inicial del programa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit', // Utilizar el modo JIT para una compilación más rápida y eficiente
+  purge: {
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  }, // Configurar PurgeCSS para eliminar estilos no utilizados en producción
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBeige: '#FFF4E5',
+        customOrange: '#EE8E11',
+        customBrown: '#5F3907',
+        customDarkestBrown: '#301C03',
+        // Puedes agregar más colores personalizados según tus necesidades
+      },      
+    }, // Extender el tema predeterminado de Tailwind CSS si es necesario
   },
-  plugins: [],
-}
-
+  plugins: [], // No hay plugins adicionales por ahora, pero puedes agregarlos si lo necesitas
+};
